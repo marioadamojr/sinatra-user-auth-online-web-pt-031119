f78d8077592b5b00249b59883d8157197293808f
Mario Adamo Jr <marioadamojr@gmail.com>
2019-06-30 14:21:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post '/registrations' do
     # puts params
-    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    @user = User.new(name: params[:name], email: params["email"], password: params["password"])
     @user.save
     session[:user_id] = @user.id
 
